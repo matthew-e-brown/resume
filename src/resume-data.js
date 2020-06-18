ccca7020fe50f219f2a5495fb77f6a2b859b3efc
Matthew Brown <matthew.e.brown.17@gmail.com>
2020-06-18 03:26:00 -0400

    Add Skills section to resume-data

--- src/resume-data.js
+++ src/resume-data.js
@@ -21,5 +21,43 @@ export default {
     //   url: "https://stackoverflow.com/users/10549827/matthew-e-brown",
     //   display: "/matthew-e-brown"
     // },
+  },
+  skills: {
+    "Programming": {
+      "Languages / General": [
+        "Two most skilled languages are JavaScript and Python",
+        "Intermediate to expert range experience working with Python for scripting",
+        [
+          // subarray will make <ul> inside previous item
+          "Intermediate experience with Pygame"
+        ],
+        "Experience with Bash/Shell scripting",
+        "Moderately experienced with C and C#",
+        "Beginner experience and moderate familiarity with Java"
+      ],
+      "Web Development Specific": [
+        "Expert experience with HTML, CSS, and JavaScript",
+        [
+          "Intermediate experience with Flexbox, experience with CSS Grid",
+          "ECMAScript ES7+ knowhow and understanding",
+          "Beginner with VueJS (this resume is a PDF of [a Vue app](https://github.com/matthew-e-brown/Resume)!)"
+        ],
+        "Experience with JavaScript for NodeJS/Server-side",
+        [
+          "Beginner with ExpressJS"
+        ],
+        "Experience and familiarity with PHP"
+      ]
+    },
+    "Software": {
+      "Linux, Git and Github": [
+        "Experience working with Linux VMs/webservers (Ubuntu, CentOS7, LAMP)",
+        "Very comfortable on the command line, including with Git",
+        "Intermediate/Advanced experience with GitHub for collaboration"
+      ],
+      "Other Software": [
+        "Atlassian, Microsoft Office, and Creative Cloud suites"
+      ]
+    }
   }
 }

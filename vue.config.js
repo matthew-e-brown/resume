802771b28bcf58ceef07a8b33c0fbe91c735e0dc
Matthew Brown <matthew.e.brown.17@gmail.com>
2020-06-20 16:39:41 -0400

    Add publicPath to Vue config
    - Without ./ as the publicPath, the built application tries to reference
      build files from .github.io's root instead of from the repository's
      subfolder.

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,7 @@ const PrerenderSpaPlugin = require('prerender-spa-plugin');
 const path = require('path');
 
 module.exports = {
+  publicPath: './',
   configureWebpack: () => {
     if (process.env.NODE_ENV !== 'production') return;
     else return {

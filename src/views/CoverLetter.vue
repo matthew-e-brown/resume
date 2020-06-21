07e41c0e64ce1d0e239f44eed09b25314fda0e6a
Matthew Brown <matthew.e.brown.17@gmail.com>
2020-06-21 09:28:17 -0400

    Implement dynamic loading of cover-letter.md

--- src/views/CoverLetter.vue
+++ src/views/CoverLetter.vue
@@ -1,11 +1,32 @@
 <template>
   <main>
-    <p>I'll write a cover letter here later.</p>
+    <p v-html="bodyText"></p>
   </main>
 </template>
 
 <script>
 export default {
-  name: 'CoverLetter'
+  name: 'CoverLetter',
+  data: function() {
+    return {
+      greeting: '',
+      bodyText: '',
+      signOff: '',
+    }
+  },
+  mounted: function() {
+    try {
+      this.bodyText = require('@/cover-letter.md').default;
+    } catch (err) {
+      this.bodyText = `
+        Could not load \`src/cover-letter.md\`. Did you create the file?
+      `;
+      return;
+    }
+
+    if (this.bodyText == '') {
+      this.bodyText = `There's no text in \`src/cover-letter.md\`.`;
+    }
+  }
 }
 </script>
